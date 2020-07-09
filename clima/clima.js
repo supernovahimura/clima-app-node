9c9c4e0d37abeb11be7360931f84862c15b4272a
Edwin Garcia <cssystem10@gmail.com>
2020-07-09 14:11:49 -0500

    change token

--- clima/clima.js
+++ clima/clima.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
-
+const token = '';
 const getClima = async(lat, lng) => {
-    const resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=a00292ede0df5f56e4fef738fdd6dc57&units=metric`)
+    const resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=${token}&units=metric`)
     return resp.data.main.temp
 
 }
